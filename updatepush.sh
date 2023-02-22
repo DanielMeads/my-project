eff4aedc52cae9a27ae05611ab60c94efbf9cf18
DanielMeads <117637190+DanielMeads@users.noreply.github.com>
2023-02-23 00:22:19 +0100

    added needs

--- updatepush.sh
+++ updatepush.sh
@@ -3,9 +3,8 @@ echo "We wrote it in Bash."
 echo "Bash is just a programming language, really."
 # This is a comment.
 # Let's read a file.
-cat some_file.txt
+cat requirements.txt
 # Do something extremely useful for continous deployment
-cd my-project
 git pull
 # Hmmm, we should probably restart the application after we've pulled in
 # new code, otherwise we can be looking for what went wrong for quite a
