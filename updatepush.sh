cceb6a3b5ed590243d7097bfaa19430829ddbb63
DanielMeads <117637190+DanielMeads@users.noreply.github.com>
2023-02-23 14:51:00 +0100

    update with readme and removal of unnessecary commands

--- updatepush.sh
+++ updatepush.sh
@@ -1,12 +1,3 @@
-echo "Here is some code."
-echo "We wrote it in Bash."
-echo "Bash is just a programming language, really."
-# This is a comment.
-# Let's read a file.
-cat requirements.txt
-# Do something extremely useful for continous deployment
 git pull
-# Hmmm, we should probably restart the application after we've pulled in
-# new code, otherwise we can be looking for what went wrong for quite a
-# while..
 systemctl restart my-project
+echo 'my-project is updated'
